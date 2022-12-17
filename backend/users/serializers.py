c1ad6961751981a4ab5ba4bc6fea6c91a0b25c12
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2022-12-17 02:42:43 +0200

    added user update functionality

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,10 +3,14 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(read_only=True)
+    delete_image = serializers.BooleanField(write_only=True)
+
     class Meta:
         model = User
         fields = (
             'full_name',
             'email',
-            'picture'
+            'picture',
+            'delete_image'
         )
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,15 +1,18 @@
+from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.views import APIView
 from rest_framework import (
     status,
     generics
 )
-from rest_framework.response import Response
-from rest_framework import permissions
-from rest_framework.views import APIView
+
 from knox.models import AuthToken
 
 from .utils import google_parse_id_token, download_user_picture
 
 from .models import User
+
 from .serializers import UserSerializer
 
 
@@ -23,14 +26,35 @@ class GoogleAuth(APIView):
             picture = download_user_picture(url=picture_url)
             user_in_db.picture.save(name=picture.name, content=picture)
         data = {'token': AuthToken.objects.create(user_in_db)[1]}
-        print(data)
-        response = Response(data=data, status=status.HTTP_200_OK)
+        response = Response(data=data, status=status.HTTP_202_ACCEPTED)
         return response
 
 
-class CurrentUserView(generics.RetrieveAPIView):
+class CurrentUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [MultiPartParser]
+    http_method_names = ['get', 'post']
 
     def get_object(self):
         return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.request.user
+
+        if serializer.validated_data.pop('delete_image'):
+            user.picture.delete()
+
+        picture = serializer.validated_data.pop('picture', None)
+        if picture:
+            user.picture = picture
+
+        for attr, val in serializer.validated_data.items():
+            setattr(user, attr, val)
+
+        user.save()
+
+        response = {'data': 'Profile updated successfully.'}
+        return Response(data=response, status=status.HTTP_200_OK)
