ab7227fac3b889a7e6e50586f352f6a0a090ad25
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2022-12-24 04:39:26 +0200

    fixed 'method not allowed' error

--- backend/users/views.py
+++ backend/users/views.py
@@ -34,7 +34,6 @@ class CurrentUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
     parser_classes = [MultiPartParser]
-    http_method_names = ['GET', 'POST']
 
     def get_object(self):
         return self.request.user
