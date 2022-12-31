9cbaee494f9f6209b6f836009fafb2ca29aba2a9
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2022-12-31 16:08:59 +0200

    fixed genres

--- backend/shows/models/genre.py
+++ backend/shows/models/genre.py
@@ -4,15 +4,19 @@ from .show import Show
 
 class Genre(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
 
     category = models.IntegerField(
         choices=Show.Category.choices,
         blank=False
     )
 
+    def get_category(self) -> Show.Category:
+        return Show.Category(self.category).label
+
     class Meta:
         db_table = 'genre'
+        unique_together = ['name', 'category']
 
     def __str__(self):
-        return f'{self.category}: {self.name}'
+        return f'{self.get_category()}: {self.name}'
